9476625529256007e875d6947ae61a836f6f2225
loic-grosjean966 <loicgrosjean96@gmail.com>
2025-12-19 22:28:43 +0100

    Refonte du site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,76 @@
-//Initialisation des tooltips de la page
-let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-let tooltipList = tooltipTriggerList.map(function(tooltipTriggerE1) { return new bootstrap.Tooltip(tooltipTriggerE1)});
+ $(document).ready(function() {
+            // Smooth scrolling pour les liens de navigation
+            $('a[href^="#"]').on('click', function(e) {
+                e.preventDefault();
+                const target = $(this.getAttribute('href'));
+                if (target.length) {
+                    $('html, body').stop().animate({
+                        scrollTop: target.offset().top - 70
+                    }, 1000);
+                }
+            });
+
+            // Animation de la navbar au scroll
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > 50) {
+                    $('.navbar').addClass('scrolled');
+                    $('#scrollTop').addClass('show');
+                } else {
+                    $('.navbar').removeClass('scrolled');
+                    $('#scrollTop').removeClass('show');
+                }
+
+                // Animation des éléments au scroll
+                $('.fade-in-up').each(function() {
+                    const elementTop = $(this).offset().top;
+                    const viewportBottom = $(window).scrollTop() + $(window).height();
+                    
+                    if (elementTop < viewportBottom - 100) {
+                        $(this).css('animation-play-state', 'running');
+                    }
+                });
+            });
+
+            // Bouton scroll to top
+            $('#scrollTop').click(function() {
+                $('html, body').animate({scrollTop: 0}, 800);
+            });
+
+            // Animation des barres de compétences
+            function animateSkills() {
+                $('.progress-bar').each(function() {
+                    const bar = $(this);
+                    const width = bar.data('width');
+                    bar.animate({width: width + '%'}, 1500, function() {
+                        bar.text(width + '%');
+                    });
+                });
+            }
+
+            // Déclenchement de l'animation des compétences au scroll
+            let skillsAnimated = false;
+            $(window).scroll(function() {
+                const skillsSection = $('#competences');
+                if (skillsSection.length) {
+                    const skillsTop = skillsSection.offset().top;
+                    const viewportBottom = $(window).scrollTop() + $(window).height();
+                    
+                    if (viewportBottom > skillsTop && !skillsAnimated) {
+                        animateSkills();
+                        skillsAnimated = true;
+                    }
+                }
+            });
+
+            // Gestion du formulaire de contact
+            $('.contact-form').submit(function(e) {
+                e.preventDefault();
+                alert('Merci pour votre message ! Je vous répondrai bientôt.');
+                this.reset();
+            });
+
+            // Animation au chargement de la page
+            setTimeout(function() {
+                $('.fade-in-up').css('animation-play-state', 'running');
+            }, 100);
+        });
